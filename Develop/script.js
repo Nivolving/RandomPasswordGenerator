b3d5fe20f2f5c3b1325a1e54a5917fb0c9450d61
Nivolving <nivethapartheeban@gmail.com>
2020-04-23 17:56:19 -0700

    altered the code sequence

--- Develop/script.js
+++ Develop/script.js
@@ -3,43 +3,11 @@ var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ['!', '@', "#", '$', '%', '^', '&', '*', '(', ')'];
-var generateBtn = document.querySelector("#generate");
 
-
-// Write password to the #password input
-
-
-//function to get the length and validate the length if its between 8 and 128
-var getLength = 0;
 function validatePasswordLength(getLength) {
 
   return (getLength >= 8 && getLength <= 128);
-
 }
-while (!validatePasswordLength(getLength)) {
-
-  getLength = parseInt(prompt("Enter a valid number between 8 and 128"));
-}
-console.log(validatePasswordLength(getLength));
-
-//var lowerCase = confirm("Would you want lowercase in your password?");
-//var upperCase = confirm("Would you want Uppercase in your password?");
-//var specialCharacters = confirm("Would you want Specialcharacters password?");
-//var numericValues = confirm("Would you want numeric value in your password?");
-
-//initializing objects to access the i/p
-var passwordOptions = {
-  "getLength": getLength,
-  "lowerCase": confirm("Would you want lowercase in your password?"),
-  "upperCase": confirm("Would you want Uppercase in your password?"),
-  "specialCharacters": confirm("Would you want Specialcharacters password?"),
-  "numericValues": confirm("Would you want numeric value in your password?"),
-
-
-};
-console.log(passwordOptions);
-
-//validate the i/p's to generate the password
 
 function generatePassword(passwordOptions) {
   var passwordFinal = '';
@@ -70,32 +38,40 @@ function generatePassword(passwordOptions) {
   }
   //console.log(passwordCharacters);
 
-  
 
-  for ( var i=0; i < passwordOptions.getLength ; i++)
-  {
 
-    passwordFinal += passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)];
-    
-    
+  for (var i = 0; i < passwordOptions.getLength; i++) {
 
+    passwordFinal += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
   }
   return passwordFinal;
   //console.log(passwordFinal);
-  
+
 }
-generatePassword(passwordOptions);
 
-
-
-// Write password to the #password input
 function writePassword() {
+  var getLength = 0;
+  while (!validatePasswordLength(getLength)) {
+
+    getLength = parseInt(prompt("Enter a valid number between 8 and 128"));
+  }
+  
+
+  var passwordOptions = {
+    "getLength": getLength,
+    "lowerCase": confirm("Would you want lowercase in your password?"),
+    "upperCase": confirm("Would you want Uppercase in your password?"),
+    "specialCharacters": confirm("Would you want Specialcharacters password?"),
+    "numericValues": confirm("Would you want numeric value in your password?"),
+
+  };
   var password = generatePassword(passwordOptions);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+//---------------------------------------------------------------------------------//
+
+var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
